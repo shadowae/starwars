6bb196a92a786369f3a16ab4f835bddb33d48d6f
Satish K <satish.appanasamy@gmail.com>
2023-08-07 18:23:11 +0800

    feature: added in data charts for people - 3 pie charts and 1 bar charts

--- src/People.tsx
+++ src/People.tsx
@@ -1,10 +1,13 @@
 import React, {useEffect} from 'react';
 import Accordion from '@mui/material/Accordion';
-import {AccordionDetails, AccordionSummary, Container, Typography} from '@mui/material';
+import {AccordionDetails, AccordionSummary, Container, Paper, Typography} from '@mui/material';
 import useStarwarsStore from './Zustand/StarwarsStore';
 import PersonalData from './components/PersonalData';
 import PeopleType from './types/PeopleType';
 import './People.css';
+import Stack from '@mui/material/Stack';
+import SimplePieChart from './components/SimplePieChart';
+import {SimpleBarChart} from './components/SimpleBarChart';
 
 const People = () => {
 	const { peopleData, fetchVehicle, fetchStarship } = useStarwarsStore(
@@ -15,9 +18,22 @@ const People = () => {
 	}, [fetchVehicle, fetchStarship]);
 	return (
 		<Container sx={{backgroundColor: '#E5E5E5'}} maxWidth="xl">
-			<Typography variant={'h4'}>
-                Star Wars - People
-			</Typography>
+			<Stack spacing={3}>
+				<Typography variant={'h4'}>
+					Star Wars - People
+				</Typography>
+			</Stack>
+			
+			<Stack useFlexGap direction={'row'} spacing={2} paddingY={'15px'}>
+				<SimplePieChart label={'Gender Breakdown'} dataSet={peopleData} path={['gender']}/>
+				<SimplePieChart label={'Eye Color Breakdown'} dataSet={peopleData} path={['eye_color']}/>
+				<SimplePieChart label={'Skin Color Breakdown'} dataSet={peopleData} path={['skin_color']}/>
+			</Stack>
+			<Container>
+				<Paper sx={{width: '90%', border: '5px dashed aquamarine', marginX: 'auto'}}>
+					<SimpleBarChart dataSet={peopleData} dataPoint={['height', 'mass']}/>
+				</Paper>
+			</Container>
 			{peopleData.map((person: PeopleType, index: number) => (
 				<Accordion key={index}>
 					<AccordionSummary>
